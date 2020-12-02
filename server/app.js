e567ed3c1f722a4b105740e2361148cf451fb648
Bruce Zhang (RD-CN) <bruce_zhang@trendmicro.com>
2020-12-02 11:50:59 +0800

    add apps

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
-const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 const app = express()
 
 const port = 3000;
 
+app.use(cors());
+
 app.get('/importmap.json', (req, res) => {
-  
+  res.json({
+    imports: {
+      '@wc/bar-chart': 'http://localhost:3000/apps/bar-chart/wc-bar-chart.js',
+      '@wc/pie-chart': 'http://localhost:3000/apps/pie-chart/wc-pie-chart.js',
+    }
+  });
 });
+
+app.use('/apps', express.static(path.resolve(__dirname, './apps')));
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+})
