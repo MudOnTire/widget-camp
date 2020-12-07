82428ad6201569008c461d317c675b28dc823723
Bruce Zhang (RD-CN) <bruce_zhang@trendmicro.com>
2020-12-07 16:40:59 +0800

    add pie chart

---
+++ apps/pie-chart/webpack.config.js
@@ -0,0 +1,17 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "wc",
+    projectName: "pie-chart",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return webpackMerge.smart(defaultConfig, {
+    externals: {
+      'highcharts': 'highcharts'
+    }
+  });
+};
